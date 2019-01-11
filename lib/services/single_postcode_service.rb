001ef174ff8f6d9686ad85ce9e8ba7fd536d545f
Abinu <asivabalan@spartaglobal.com>
2019-01-11 15:27:29 +0000

    tests and readme updated

--- lib/services/single_postcode_service.rb
+++ lib/services/single_postcode_service.rb
@@ -11,12 +11,72 @@ class SinglePostcodeService
     @single_postcode_data = JSON.parse(self.class.get("/postcodes/#{postcode}").body)
   end
 
- def get_status_code
+  def get_status_code
    @single_postcode_data['status']
- end
+  end
 
- def get_results
+  def get_results
    @single_postcode_data['result']
- end 
+  end
+
+  def get_postcode
+   @single_postcode_data["result"]["postcode"].gsub!(/\s/,'')
+  end
+
+  def get_quality_integer
+    @single_postcode_data["result"]["quality"]
+  end
+
+  def get_ordnance_survery_eastings
+    @single_postcode_data["result"]["eastings"]
+  end
+
+  def get_ordnance_survery_northings
+    @single_postcode_data["result"]["northings"]
+  end
+
+  def get_four_constituent_countrys
+    @single_postcode_data["result"]["country"]
+  end
+
+  def get_NHS_authroity
+    @single_postcode_data["result"]["nhs_ha"]
+  end
+
+  def get_longitude_value
+    @single_postcode_data["result"]["longitude"]
+  end
+
+  def get_latitude_value
+    @single_postcode_data["result"]["latitude"]
+  end
+
+  def get_parliamentry_constituency
+    @single_postcode_data["result"]["parliamentary_constituency"]
+  end
+
+  def get_european_electoral_region
+    @single_postcode_data["result"]["european_electoral_region"]
+  end
+
+  def get_primary_care_trust
+    @single_postcode_data["result"]["primary_care_trust"]
+  end
+
+  def get_region
+    @single_postcode_data["result"]["region"]
+  end
+
+  def get_parish
+    @single_postcode_data["result"]["parish"]
+  end
+
+  def get_admin_district
+    @single_postcode_data["result"]["codes"]["admin_district"]
+  end
+
+  def get_incode
+    @single_postcode_data["result"]["incode"]
+  end
 
 end
--- spec/multiple_postcodes_spec.rb
+++ spec/multiple_postcodes_spec.rb
@@ -2,13 +2,29 @@ describe MultiplePostcodesService do
 
 #instantiate server
   before (:all) do
-    @postcodes_array = ['B601JA', 'SW155FB']
+    @postcodes_array = ["OX49 5NU", "M32 0JG", "NE30 1DP"]
     @multiple_postcodes_service = Postcodesio.new.multiple_postcodes_service
     @multiple_postcodes_service.get_multiple_postcodes(@postcodes_array)
   end
 
-  it 'should respond with a status message of 200' do
-    expect(@multiple_postcodes_service.get_status_code).to eq 200
-  end
+    it 'should respond with a status message of 200' do
+      expect(@multiple_postcodes_service.get_status_code).to eq 200
+    end
+
+    it "should return the first query as the first postcode in the response" do
+      expect(@multiple_postcodes_service.get_first_query).to be_kind_of String
+    end
+
+    it "should return the second query as the first postcode in the response" do
+      expect(@multiple_postcodes_service.get_second_query).to be_kind_of String
+    end
+
+    it "should return an integer for quantity" do
+      expect(@multiple_postcodes_service.get_quality).to be_kind_of Integer
+    end
+
+    it "should return an integer for eastings" do
+      expect(@multiple_postcodes_service.get_eastings).to be_kind_of Integer
+    end
 
 end
